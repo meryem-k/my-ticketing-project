1d5a9090ef4830e48bc5083521ec27af60b81f9a
sule11 <smkargin11@gmail.com>
2022-10-05 16:51:19 -0500

    Initial commit

---
+++ src/test/java/com/cydeo/myticketingproject/MyTicketingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.myticketingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTicketingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
