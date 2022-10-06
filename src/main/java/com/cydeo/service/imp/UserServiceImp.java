beea1a7ab7172a78c415819e5924d32ce7d8f17f
sule11 <smkargin11@gmail.com>
2022-10-05 22:42:03 -0500

    added DataGenerator for Role and User Service

--- src/main/java/com/cydeo/service/imp/UserServiceImp.java
+++ src/main/java/com/cydeo/service/imp/UserServiceImp.java
@@ -2,9 +2,11 @@ package com.cydeo.service.imp;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImp extends AbstractMapService<UserDTO, String> implements UserService {
 
 
