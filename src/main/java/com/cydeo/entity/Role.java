910802ddf767a762522d8c9a498b382bb016fca2
sule11 <smkargin11@gmail.com>
2022-11-02 10:19:45 -0500

    added annotaions in entity classes in order to create tables in DB and persist data

--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -1,16 +1,19 @@
 package com.cydeo.entity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Role {
+@Entity
+@Table(name="roles")
+public class Role extends BaseEntity{
 
-    private Long id;
     private String description;
 
 }
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -1,14 +1,4 @@
 package com.cydeo.service;
 
-import com.cydeo.dto.UserDTO;
-
-import java.util.List;
-
-public interface UserService extends CrudService<UserDTO,String> {
-
-
-    List<UserDTO> findManagers();
-
-    List<UserDTO> findEmployees();
-
+public interface UserService {
 }
