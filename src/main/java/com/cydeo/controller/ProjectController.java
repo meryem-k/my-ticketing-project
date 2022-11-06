2239031ea1771a3a1d51ad5df245107cc51fe707
sule11 <smkargin11@gmail.com>
2022-11-06 08:58:18 -0600

    added listByRole operation

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -3,6 +3,7 @@ package com.cydeo.controller;
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,26 +17,26 @@ import java.util.List;
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
-//
-//    ProjectService projectService;
-//    UserService userService;
-//
-//    public ProjectController(ProjectService projectService, UserService userService) {
-//        this.projectService = projectService;
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping("/create")
-//    public String createProject(Model model){
-//
-//        model.addAttribute("project",new ProjectDTO());
-//        model.addAttribute("projects",projectService.findAll());
-//        model.addAttribute("managers",userService.findManagers());
-//
-//
-//        return "/project/create";
-//    }
-//
+
+    ProjectService projectService;
+    UserService userService;
+
+    public ProjectController(ProjectService projectService, UserService userService) {
+        this.projectService = projectService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/create")
+    public String createProject(Model model){
+
+        model.addAttribute("project",new ProjectDTO());
+        model.addAttribute("projects",projectService.listAllProjects());
+        model.addAttribute("managers",userService.listAllByRole("manager"));
+
+
+        return "/project/create";
+    }
+
 //    @PostMapping("/create")
 //    public String insertProject(ProjectDTO project){
 //
--- src/main/java/com/cydeo/service/imp/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/imp/ProjectServiceImpl.java
@@ -1,22 +1,41 @@
 package com.cydeo.service.imp;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.entity.Project;
+import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
 
+
+    private final ProjectRepository projectRepository;
+
+    private final ProjectMapper projectMapper;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+        this.projectRepository = projectRepository;
+        this.projectMapper = projectMapper;
+    }
+
+
     @Override
     public ProjectDTO getByProjectCode(String code) {
-        return null;
+        Project project = projectRepository.findByProjectCode(code);
+        return projectMapper.convertToDto(project);
     }
 
     @Override
     public List<ProjectDTO> listAllProjects() {
-        return null;
+
+        List<Project> list = projectRepository.findAll();
+
+        return list.stream().map(projectMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
