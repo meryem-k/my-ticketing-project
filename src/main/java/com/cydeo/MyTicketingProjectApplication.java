766513f91b62d60b49351a2c7c2b1c35d0c394a2
sule11 <smkargin11@gmail.com>
2022-11-19 19:00:00 -0600

    adding security dependency and creating bea from the PasswordEncoder interface

--- src/main/java/com/cydeo/MyTicketingProjectApplication.java
+++ src/main/java/com/cydeo/MyTicketingProjectApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MyTicketingProjectApplication {
@@ -18,6 +20,10 @@ public class MyTicketingProjectApplication {
         return new ModelMapper();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 
 }
